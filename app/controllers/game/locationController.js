f594798c081d850deba1db3142301dec36825374
Cristian Cuartas <kristian_9411@hotmail.com>
2023-03-10 18:17:49 -0500

    add: location controller

---
+++ app/controllers/game/locationController.js
@@ -0,0 +1,19 @@
+const { models } = require('../../../database');
+
+async function createLocation(idAldea) {
+    try {
+        let location = await models.location.create({
+                            idMap: 1,
+                            posX: 10,
+                            posY: 10
+                        });
+
+        return location;
+
+    } catch(e) {
+        console.log(`Error: locationController - createLocation: ${e}`);
+        return null;
+    }
+}
+
+module.exports = { createLocation }
