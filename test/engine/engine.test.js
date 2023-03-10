99a262fd0dcd02bb1139dca1e32ebfb6a439d447
Cristian Cuartas <kristian_9411@hotmail.com>
2023-03-10 18:24:39 -0500

    refac: change in test engine

--- test/engine/engine.test.js
+++ test/engine/engine.test.js
@@ -3,31 +3,73 @@ const PLAYER = require('../../engine/modules/player').Player;
 const NPC = require('../../engine/modules/npc').Npc;
 
 const DATA_DB_PLAYER = {
-    ID: 1,
+    id: 1,
     name: 'prueba',
-    skinBase: 'testBase',
-    skinHair: 'H-1',
+    gender: null,
     health: 154,
     level: 1,
-    xp: 0,
+    experience: 0,
     money: 0,
-    IDMap: 1,
-    X: 16,
-    Y: 15
+    SKIN: {
+        base: 'testBase',
+        hair: 'H-1',
+    },
+    LOCATION: {
+        idMap: 1,
+        posX: 16,
+        posY: 15
+    },
+    ATTRIBUTES: {
+        strength: 1,
+    },
+    KEYBOARD: {
+        keyAction1: 81,
+        keyAction2: 87,
+        keyAction3: 69,
+        keyAction4: 82,
+        keyAction5: 84,
+        keyAction6: 65,
+        keyCharacter: 80,
+        keyBook: 76,
+        keyMenu: 27,
+        keyMap: 77,
+        keySkills: 74
+    }
 };
 
 const DATA_DB_PLAYER2 = {
-    ID: 2,
+    id: 2,
     name: 'test',
-    skinBase: 'testBase',
-    skinHair: 'H-1',
+    gender: null,
     health: 154,
     level: 1,
-    xp: 0,
+    experience: 0,
     money: 0,
-    IDMap: 1,
-    X: 16,
-    Y: 15
+    SKIN: {
+        base: 'testBase',
+        hair: 'H-1',
+    },
+    LOCATION: {
+        idMap: 1,
+        posX: 16,
+        posY: 15
+    },
+    ATTRIBUTES: {
+        strength: 1,
+    },
+    KEYBOARD: {
+        keyAction1: 81,
+        keyAction2: 87,
+        keyAction3: 69,
+        keyAction4: 82,
+        keyAction5: 84,
+        keyAction6: 65,
+        keyCharacter: 80,
+        keyBook: 76,
+        keyMenu: 27,
+        keyMap: 77,
+        keySkills: 74
+    }
 };
 
 const DATA_DB_NPC = {
@@ -113,7 +155,7 @@ test('NPCNearby', () => {
 * ------------------------------ */
 test('addPlayer', () => {
     let idPlayer = '1';
-    let posWorldPlayer = Engine.posWorld(DATA_DB_PLAYER.IDMap, DATA_DB_PLAYER.X, DATA_DB_PLAYER.Y);
+    let posWorldPlayer = Engine.posWorld(DATA_DB_PLAYER.LOCATION.idMap, DATA_DB_PLAYER.LOCATION.posX, DATA_DB_PLAYER.LOCATION.posY);
     let response = new PLAYER(idPlayer, DATA_DB_PLAYER, posWorldPlayer.x, posWorldPlayer.y, "imagen1\r\n", "");
 
 
@@ -122,10 +164,10 @@ test('addPlayer', () => {
 
 test('playersNearby', () => {
     // PLAYER 1
-    let posWorldPlayer = Engine.posWorld(DATA_DB_PLAYER.IDMap, DATA_DB_PLAYER.X, DATA_DB_PLAYER.Y);
+    let posWorldPlayer = Engine.posWorld(DATA_DB_PLAYER.LOCATION.idMap, DATA_DB_PLAYER.LOCATION.posX, DATA_DB_PLAYER.LOCATION.posX);
     let player = Engine.addPlayer('1', DATA_DB_PLAYER, posWorldPlayer.x, posWorldPlayer.y, "", "");
     // PLAYER 2
-    let posWorldPlayer2 = Engine.posWorld(DATA_DB_PLAYER.IDMap, DATA_DB_PLAYER.X, DATA_DB_PLAYER.Y);
+    let posWorldPlayer2 = Engine.posWorld(DATA_DB_PLAYER.LOCATION.idMap, DATA_DB_PLAYER.LOCATION.posX, DATA_DB_PLAYER.LOCATION.posX);
     let player2 = Engine.addPlayer('2', DATA_DB_PLAYER2, posWorldPlayer2.x, posWorldPlayer2.y, "", "");
     let response = [player2];
 
@@ -147,7 +189,7 @@ test('movePlayer', () => {
 
 test('playerDisconnect', () => {
     let idPlayer = '1';
-    let posWorldPlayer = Engine.posWorld(DATA_DB_PLAYER.IDMap, DATA_DB_PLAYER.X, DATA_DB_PLAYER.Y);
+    let posWorldPlayer = Engine.posWorld(DATA_DB_PLAYER.LOCATION.idMap, DATA_DB_PLAYER.LOCATION.posX, DATA_DB_PLAYER.LOCATION.posY);
     let response = new PLAYER(idPlayer, DATA_DB_PLAYER, posWorldPlayer.x, posWorldPlayer.y, "imagen1\r\n", "");
 
     expect(Engine.playerDisconnect(idPlayer)).toStrictEqual(response);
