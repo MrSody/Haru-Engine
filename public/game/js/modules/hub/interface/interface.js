18bbe6ef6ddb0049acf004f096c1e2260154c87c
Cristian Cuartas <kristian_9411@hotmail.com>
2023-03-10 18:20:00 -0500

    refact: interface character public

--- public/game/js/modules/hub/interface/interface.js
+++ public/game/js/modules/hub/interface/interface.js
@@ -1,24 +1,8 @@
 export default class Interface {
     constructor () {
-        this.maxPlayers = 5;
         this.maxLoadScreen = 4;
-
-        this.IDPJ;
-        this.characters = [];
     }
-
-    setIDPJ (id) {
-        this.IDPJ = id;
-    }
-
-    getIDPJ () {
-        return this.IDPJ;
-    }
-
-    getCharacters () {
-        return this.characters;
-    }
-
+    
 /*-------------------------------
     Ayuda - HUB
 *-------------------------------*/
@@ -73,59 +57,6 @@ export default class Interface {
         this.removeClass('#loading', 'Invisible');
 
         this.innerHTML('#loading', html);
-    }
-
-/*-------------------------------
-    HUB - Pantalla de personajes
-*-------------------------------*/
-    accountCharacters (count) {
-        let character = this.characters[count];
-
-        let html =  `<div class="col">
-                        <img src="../sprites/Player/Base/${character.skinBase}.png">
-                    </div>
-                    <div class="col">
-                        <div>${character.name}</div>
-                    </div>`;
-
-        return html;
-    }
-
-    // Muestra los personajes en la interface
-    onAccountCharacters (data) {
-        // Ocualta la pantalla de carga
-        this.addClass('#loading', 'Invisible');
-
-        // Muestra la pantalla de los personajes
-        this.removeClass('#character', 'Invisible');
-
-        console.log(data);
-
-        let html = "";
-
-        this.selCharacter(data[0].ID, data[0].Skin_Base, data[0].Name);
-
-        let count = 0;
-        data.forEach((Pj) => {
-
-            this.characters.push({ID: Pj.ID, skinBase: Pj.Skin_Base, name: Pj.Name});
-
-            this.addClass(`#Pj_${count}`, 'bgCharacter');
-            this.innerHTML(`#Pj_${count}`, this.accountCharacters(count));
-            count++;
-        });
-
-        for (let count = data.length; count < this.maxPlayers; count++) {
-            this.addClass(`#Pj_${count}`, 'bgNewCharacter');
-        }
-    }
-
-    selCharacter (ID, Skin_Base, Name) {
-        // Save ID pj active
-        this.setIDPJ(ID);
-        
-        this.innerHTML('#characters_Skin', `<div id="characters_Skin" style="margin-top: 15%; margin-left: 40%;"><img src="../sprites/Player/Base/${Skin_Base}.png" style="width: 150px;"></div>`);
-        this.innerHTML('#characters_Name', `Nombre: ${Name}`);
     }
 
 /*-------------------------------
