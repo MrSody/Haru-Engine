826d48f14fe1af8c53b8b8c51daf11125ea65dbc
Cristian Cuartas <kristian_9411@hotmail.com>
2023-12-16 15:16:12 -0500

    add: helper directions

--- public/game/js/modules/helper.js
+++ public/game/js/modules/helper.js
@@ -6,4 +6,17 @@ export default class Helpers {
 
         return imagen;
     }
+
+    /**
+     * 
+     * @return @enum {number}
+     */
+    static directions () {
+        return Object.freeze({
+                    Up: 0,
+                    Right: 1,
+                    Down: 2,
+                    Left: 3,
+                });
+    }
 }
