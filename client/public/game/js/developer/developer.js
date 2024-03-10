3e803ef2f7b093369085c342ee16b4d14159d038
Cristian Cuartas <kristian_9411@hotmail.com>
2024-03-10 12:54:47 -0500

    doc: add developer

--- client/public/game/js/developer/developer.js
+++ client/public/game/js/developer/developer.js
@@ -1,47 +1,73 @@
 export default class Developer {
+    /**
+     * @constructor
+     * @param {number} tileSize
+    */
     constructor (tileSize) {
+
+        /** @type {number} */  
         this.tileSize = tileSize;
+
+        /** @type {number} */
         this.lineSize = 0.4;
+
+        /** @type {string} */
         this.lineColor = "#44414B";
     }
 
-    drawGrid (ctx, cXnull, cYnull) {
+    /**
+     * @param {any} ctx
+     * @param {number} X
+     * @param {number} Y
+     */
+    drawGrid (ctx, X, Y) {
         ctx.strokeStyle = this.lineColor;
         ctx.lineWidth = this.lineSize;
 
-        // VERTICALES
+        // VERTICAL
         ctx.beginPath();
-        ctx.moveTo((cXnull * this.tileSize), 0);
-        ctx.lineTo((cXnull * this.tileSize), ctx.canvas.height);
+        ctx.moveTo((X * this.tileSize), 0);
+        ctx.lineTo((X * this.tileSize), ctx.canvas.height);
         ctx.stroke();
 
-        // HORIZONTALES
+        // HORIZONTAL
         ctx.beginPath();
-        ctx.moveTo(0, ((cYnull - 0.5) * this.tileSize));
-        ctx.lineTo(ctx.canvas.width, ((cYnull - 0.5) * this.tileSize));
+        ctx.moveTo(0, ((Y - 0.5) * this.tileSize));
+        ctx.lineTo(ctx.canvas.width, ((Y - 0.5) * this.tileSize));
         ctx.stroke();
     }
 
-    drawCollision (ctx, cXnull, cYnull, mapCollision) {
+    /**
+     * @param {any} ctx
+     * @param {number} X
+     * @param {number} Y
+     * @param {Array<Array>} mapCollision
+     */
+    drawCollision (ctx, X, Y, mapCollision) {
         
-        if ( typeof(mapCollision[cYnull][cXnull]) == "number" ) {
-            if ( mapCollision[cYnull][cXnull] == 1 ) { // COLLISION
+        if ( typeof(mapCollision[Y][X]) == "number" ) {
+            if ( mapCollision[Y][X] == 1 ) { // COLLISION
                 ctx.fillStyle = "rgba(255, 0, 0, 0.5)";
-                ctx.fillRect((cXnull * this.tileSize), ((cYnull - 0.5) * this.tileSize), 32, 32);
+                ctx.fillRect((X * this.tileSize), ((Y - 0.5) * this.tileSize), 32, 32);
             }
-        } else if ( typeof(mapCollision[cYnull][cXnull]) == "object" ) {
+        } else if ( typeof(mapCollision[Y][X]) == "object" ) {
 
-            let npc = mapCollision[cYnull][cXnull];
+            let npc = mapCollision[Y][X];
 
             switch ( npc.getReaction() ) {
                 case 2: // NPC - Aggressive
                 ctx.fillStyle = "rgba(0, 0, 255, 1)";
-                ctx.fillRect((cXnull * this.tileSize), ((cYnull - 0.5) * this.tileSize), 32, 32);
+                ctx.fillRect((X * this.tileSize), ((Y - 0.5) * this.tileSize), 32, 32);
                 break;
             }
         }
     }
 
+    /**
+     * @param {any} ctx
+     * @param {number} visionDistance
+     * @param {{x: number; y: number}} posNow
+     */
     drawVisionNpc (ctx, visionDistance, posNow) {
         let initVisionX = posNow.x - visionDistance;
         let initVisionY = posNow.y - visionDistance;
